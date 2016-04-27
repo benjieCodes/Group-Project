58dd70d7cc03d970fa22ac236b243e81d45c4cff
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-27 16:54:36 -0400

    replaced img sources with # so that console won't log errors and also pulled API for random user pictures

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-import $ from 'jquery';
+import $ from "jquery";
+
+
+// Working on the Testimony div
+var randomUserBaseURL = "http://api.randomuser.me/";
+var ruUserAmount = "?results=3"; // Pulling 3 users
+var ruFinalURL = randomUserBaseURL + ruUserAmount;
+
+// Pulling API using .getJSON
+$.getJSON(ruFinalURL).then(function (response){
+  console.log(response);
+    response.results.forEach(function (res){
+      console.log(res.picture.large);
+    })
+
+});
