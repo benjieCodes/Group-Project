49489be149fe6eb9a98dd70d3e955559957ebf14
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-27 21:32:30 -0400

    Worked on Testimonies div and added api to page, however need to fix name and review

--- src/js/main.js
+++ src/js/main.js
@@ -6,16 +6,40 @@ var randomUserBaseURL = "http://api.randomuser.me/";
 var ruUserAmount = "?results=3"; // Pulling 3 users
 var ruFinalURL = randomUserBaseURL + ruUserAmount;
 
+var testimonyURL = 'https://json-data.herokuapp.com/darts/testimonials';
+
 // Pulling API using .getJSON
 $.getJSON(ruFinalURL).then(function (response){
-  console.log(response);
     response.results.forEach(function (res){
-      console.log(res.picture.large);
+      var html = getUser(res);
+      $('.Testimonial').append(html);
     })
+$.getJSON(testimonyURL).then(function (response){
+  console.log(response);
+    response.results.filter(function (res){
+      var html = getTestimony(res);
+      $('.users').append(html);
+    })
+})
 
 });
 
+function getUser (user) {
+  return `
+          <ul class="users">
+          <li><img src=${user.picture.large}></li>
+          </ul>
+  `;
+}
 
-<!----------------------------------------------
+function getTestimony (testimony) {
+  return `
+          <li>${testimony.name}</li>
+          <li>${testimony.review}</li>
+  `;
+}
+
+
+
 // Don't know who's work this is but I kept it on there.
 $('.carousel').carousel();
