5d7722926bbf5aa73287a4d11f59edf98c993c8f
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-28 11:02:28 -0400

    Continued to work on testimonials and successfully able to add an array for users and the testimonies

--- src/js/main.js
+++ src/js/main.js
@@ -3,43 +3,101 @@ import $ from "jquery";
 // BENJIES CURRENT WORK FOR THE TESTIMONY DIV
 // DO NOT ERASE
 var randomUserBaseURL = "http://api.randomuser.me/";
-var ruUserAmount = "?results=3"; // Pulling 3 users
-var ruFinalURL = randomUserBaseURL + ruUserAmount;
+var ruGenders = ["?gender=female","?gender=male","?gender=male"];
+var ruURL = randomUserBaseURL;
 
 var testimonyURL = 'https://json-data.herokuapp.com/darts/testimonials';
 
-// Pulling API using .getJSON
-$.getJSON(ruFinalURL).then(function (response){
-    response.results.forEach(function (res){
-      var html = getUser(res);
-      $('.Testimonial').append(html);
-    })
-$.getJSON(testimonyURL).then(function (response){
-  console.log(response);
-    response.results.filter(function (res){
-      var html = getTestimony(res);
-      $('.users').append(html);
-    })
-})
+// variable used to push each user image to this array
+var users = [];
 
+setTimeout(function (){
+    console.log('users', users);
+}, 1000);
+// Made a function in order to be able to get the correct gender order each time
+ruGenders.forEach (function (gender){
+    var ruFinalURL = ruURL + gender;
+    $.getJSON(ruFinalURL).then(function(response){
+      var user = response.results[0];
+      users.push(user)
+    });
 });
+// variable used to push each testimonial object to this array
+var testimonials = [];
 
-function getUser (user) {
-  return `
-          <ul class="users">
-          <li><img src=${user.picture.large}></li>
-          </ul>
-  `;
-}
+// on page load
+getTestimonials();
 
-function getTestimony (testimony) {
-  return `
-          <li>${testimony.name}</li>
-          <li>${testimony.review}</li>
-  `;
-}
+// it takes longer for an API request compared to just a variable
+setTimeout( function() {
+  console.log('testimonials', testimonials);
+}, 1000);
+
+// defining function
+function getTestimonials() {
+  $.getJSON(testimonyURL).then(function(response){
+    response.results.forEach(function(testimonial){
+      testimonials.push(testimonial);
+    });
+  });
+};
 
 
+// $.getJSON(ruFinalURL).then(function (response){
+//       var users = response.results;
+//       console.log('users:', users);
+//       users.forEach(function (user){
+//         console.log(user)
+//       });
+//     });
 
-// Don't know who's work this is but I kept it on there.
-$('.carousel').carousel();
+//     $.getJSON(testimonyURL).then(function (response){
+//         var testimonials = response.results;
+//         console.log('testimonials: ', testimonials);
+//       });
+// });
+      // var html = userTemplate(user);
+      // $('.Testimonial').append(html);
+    // I want to grab each testimonial and put one testimony on each user
+    // testimonials.forEach(function (testimonial){
+//       var html = testimonyTemplate(testimonial);
+//       $('.users').append(html);
+//     })
+// })
+//
+// });
+//
+// function userTemplate (user) {
+//   return `
+//           <ul class="users">
+//           <li><img src=${user.picture.large}></li>
+//           </ul>
+//   `;
+// }
+//
+// function testimonyTemplate (testimony) {
+//   return `
+//           <li>${testimony.name}</li>
+//           <li>${testimony.review}</li>
+//   `;
+// }
+//
+// var testimonyCOunt = 0;
+// var numOfUsers = 3;
+// function placeTestimony () {
+//   if (count < numOfUsers.length) {
+//     console.log('function called');
+//     var current = testimonials.indexOf(count)
+//     console.log(current);
+//     count = count + 1;
+//     console.log(count);
+//   }
+//   else {
+//     console.log('testimonials are done');
+//   }
+// }
+//
+//
+//
+// // Don't know who's work this is but I kept it on there.
+// $('.carousel').carousel();
