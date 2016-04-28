35fe3b4d65587c655e6db443dccb09e09c293482
JEFF HENRY <henry_jf@hotmail.com>
2016-04-28 08:17:20 -0400

    removed an empty line

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,4 @@
-import $ from "jquery";
-
-// BENJIES CURRENT WORK FOR THE TESTIMONY DIV
-// DO NOT ERASE
-var randomUserBaseURL = "http://api.randomuser.me/";
-var ruUserAmount = "?results=3"; // Pulling 3 users
-var ruFinalURL = randomUserBaseURL + ruUserAmount;
-
-// Pulling API using .getJSON
-$.getJSON(ruFinalURL).then(function (response){
-  console.log(response);
-    response.results.forEach(function (res){
-      console.log(res.picture.large);
-    })
-
-});
+import $ from 'jquery';
 
 
-<!----------------------------------------------
-// Don't know who's work this is but I kept it on there.
-$('.carousel').carousel();
+$('.carousel').carousel()
